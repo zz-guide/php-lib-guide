9a30dc1baabc0df5c775428bdfba43ea7da4409c
许磊 <xulei131401@outlook.com>
2024-03-31 20:37:24 +0800

    fix

---
+++ test.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * @author:  leixu
+ * @version: 1.0.0
+ * @change:
+ *    1. 2022/8/29 leixu: 创建；
+ */
+
+//$str = "%E4%B8%AD%E5%9B%BD%E6%B2%B3%E5%8C%97%E7%9C%81%E5%94%90%E5%B1%B1%E5%B8%82%E5%8F%A4%E5%86%B6%E5%8C%BA%E4%B9%A0%E5%AE%B6%E5%A5%97%E4%B9%A1S262%28%E5%8F%A4%E6%9E%97%E8%B7%AF%29";
+//
+//echo urldecode($str);
+
+
+//$startDateIsYesterday = date("Y-m-d", strtotime("-1 day")) == '2023-10-10';
+//$specialCond = (
+//    (time() > strtotime(date('Y-m-d 00:00:00')) && time() <= strtotime(date('Y-m-d 02:40:00'))) &&
+//    $startDateIsYesterday
+//);
+//
+//var_dump($startDateIsYesterday);
+//var_dump($specialCond);
+//var_dump(time());
+//var_dump(strtotime(date('Y-m-d 00:00:00')));
+//var_dump(strtotime(date('Y-m-d 02:40:00')));
